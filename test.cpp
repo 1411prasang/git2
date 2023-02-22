d5e4df2c566d235c12cf27958a9fe6f623e1bbf0
Prasang Agarwal <prasang.agarwal@qbitlabs.co.in>
2023-02-22 17:29:05 +0530

    Added Test

---
+++ test.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+float divide(int a,int b){
+    return a/b;
+}
+
+int add(int a,int b,int c){
+    return a+b+c;
+int add(int a,int b,int c,int d,int e){
+    return a+b+c+d+e;
+}
+int tanX(int a){
+    return tan(a);
+}
+//////////////////Ho gua bssss//////////
+int multiply(int a,int b,int c,int d,int e){
+    d=e*d
+    a=d
+    return a*b*c;
+}
+float secX(int a) {
+    return 1/cos(a);
+}
+int multiply(int a,int b){
+    return a*b;
+}
+float sinx(int x){
+    return sin(x);
+}
+float cosx(int x){
+    return cos(x);
+}
+float quotient(int a,int b){
+    return a/b;
+}
+int main(){
+    cout<<quotient(3,1);
+    return 0;
+
+}
